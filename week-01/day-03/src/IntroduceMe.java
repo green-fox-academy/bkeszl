624e16da70bc8be54f250757c169af26c615a3d1
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-05 13:56:38 +0100

    added ItroduceMe (ex.4)

---
+++ week-01/day-03/src/IntroduceMe.java
@@ -0,0 +1,20 @@
+public class IntroduceMe {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+        String name ="Keszler Barnabás";
+        int age = 25;
+        float height = 1.97f;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
