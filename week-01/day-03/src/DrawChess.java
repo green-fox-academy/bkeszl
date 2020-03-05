6b39664f981b1bff5a4ca2ca4e93aefef2427eab
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-05 17:30:03 +0100

    added chess table drawer

---
+++ week-01/day-03/src/DrawChess.java
@@ -0,0 +1,8 @@
+public class DrawChess {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println(" % % % %");
+            System.out.println("% % % %");
+        }
+    }
+}
