72b61dbe2f9e72d573c4b1394ae99645eec23ef1
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-09 14:28:01 +0100

    added animals to colors

--- week-02/day-01/src/Colors.java
+++ week-02/day-01/src/Colors.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
+
 public class Colors {
     public static void main(String[] args) {
-        String[][] Colors = {
+        String[][] colors = {
                 {"lime", "forest green", "olive", "pale green", "spring green"},
                 {"orange red", "red", "tomato"},
                 {"orchid", "violet", "pink", "hot pink"},
         };
+        String[] animals = {"koal", "pand", "zebr"};
+        for (int i = 0; i <animals.length ; i++) {
+            animals[i]=animals[i].concat("a");
+        }
+        System.out.println(Arrays.toString(animals));
     }
 }
