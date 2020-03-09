0fc2cc5abec4780960a931d03dcdd822098a68c9
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-09 14:33:46 +0100

    added orders

---
+++ week-02/day-01/src/Orders.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Orders {
+    public static void main(String[] args) {
+        String[] orders = {"first", "second", "third"};
+        String buffer = new String();
+
+        buffer =  orders[0];
+        orders[0] = orders[2];
+        orders[2] = buffer;
+
+        System.out.println(Arrays.toString(orders));
+    }
+}
