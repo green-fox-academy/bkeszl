76b22e8d773d0560857130900592a2c1377616fc
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-09 13:39:55 +0100

    added increment element

---
+++ week-02/day-01/src/IncrementElement.java
@@ -0,0 +1,7 @@
+public class IncrementElement  {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        array[2]++;
+        System.out.println(array[2]);
+    }
+}
