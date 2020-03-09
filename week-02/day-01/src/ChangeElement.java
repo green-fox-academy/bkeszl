ee079f3d21e1f9bc18bf219caf9d164b5a4b32b4
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-09 14:10:36 +0100

    changed change element to use a loop

--- week-02/day-01/src/ChangeElement.java
+++ week-02/day-01/src/ChangeElement.java
@@ -1,7 +1,14 @@
 public class ChangeElement {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 8, 5, 6};
-        array[3] = 4;
-        System.out.println(array[3]);
+        int index =0 ;
+        for (int i = 0; i <array.length ; i++) {
+            if (array[i]==8){
+                array[i]=4;
+                index = i;
+            }
+        }
+
+        System.out.println(array[index]);
     }
 }
