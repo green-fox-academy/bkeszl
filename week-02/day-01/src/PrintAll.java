5b4d8d6870866c8fb98b551eccd8c953f8e6ff69
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-09 13:51:10 +0100

    added PrintAll

---
+++ week-02/day-01/src/PrintAll.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+
+public class PrintAll {
+    public static void main(String[] args) {
+        int[] array = {4, 5, 6, 7};
+        for (int n:array) {
+            System.out.println(n);
+        }
+    }
+}
