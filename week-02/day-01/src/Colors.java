ae39e4c48681fe28286b1040f161854c7ff54ad4
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-09 14:21:29 +0100

    added COlors

---
+++ week-02/day-01/src/Colors.java
@@ -0,0 +1,9 @@
+public class Colors {
+    public static void main(String[] args) {
+        String[][] Colors = {
+                {"lime", "forest green", "olive", "pale green", "spring green"},
+                {"orange red", "red", "tomato"},
+                {"orchid", "violet", "pink", "hot pink"},
+        };
+    }
+}
