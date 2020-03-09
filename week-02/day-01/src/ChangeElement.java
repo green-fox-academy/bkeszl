9fe5cd6bd9884bce620ef10143e0005978457b48
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-09 13:32:33 +0100

    added change element

---
+++ week-02/day-01/src/ChangeElement.java
@@ -0,0 +1,7 @@
+public class ChangeElement {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 8, 5, 6};
+        array[3] = 4;
+        System.out.println(array[3]);
+    }
+}
