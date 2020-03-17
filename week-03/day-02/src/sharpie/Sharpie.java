bec0c579dd305fcb30bf28ff15e4c5b40a737651
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-03-17 14:56:03 +0100

    added sharpie excercise

---
+++ week-03/day-02/src/sharpie/Sharpie.java
@@ -0,0 +1,24 @@
+package sharpie;
+
+public class Sharpie {
+  String color;
+  float width;
+  float inkAmount;
+
+  public Sharpie(String color, float width) {
+    this.color = color;
+    this.width = width;
+    inkAmount = 100;
+  }
+
+  public void use() {
+    inkAmount--;
+  }
+
+  public static void main(String[] args) {
+    Sharpie redSharpie = new Sharpie("Red", 2.35f);
+    redSharpie.use();
+    System.out.println(redSharpie.inkAmount);
+    System.out.println(redSharpie.color);
+  }
+}
