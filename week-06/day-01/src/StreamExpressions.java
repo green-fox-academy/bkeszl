a4f01b73ece710bf34a310db80781f34abdca280
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-04-14 14:02:40 +0200

    exercise 3 - sqrt

--- week-06/day-01/src/StreamExpressions.java
+++ week-06/day-01/src/StreamExpressions.java
@@ -3,8 +3,8 @@ import java.util.List;
 
 public class StreamExpressions {
   public static void main(String[] args) {
-    System.out.println("Exercise 1");
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    System.out.println("Exercise 1");
     numbers.stream()
         .filter(number -> number % 2 == 0)
         .forEach(System.out::println);
@@ -13,6 +13,12 @@ public class StreamExpressions {
     numbers.stream()
         .filter(number -> 0 < number)
         .map(number -> number * number)
+        .forEach(System.out::println);
+
+    System.out.println("Exercise 3");
+    numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
+    numbers.stream()
+        .filter(number -> Math.sqrt(20) < number)
         .forEach(System.out::println);
   }
 }
