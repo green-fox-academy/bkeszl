4f74f42e8e7d116d5bb72153b773f4679627cb68
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-04-16 15:47:15 +0200

    added name parameter to hellowebcontroller

--- week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,12 +3,13 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam String name) {
+    model.addAttribute("name", name);
     return "greeting";
   }
 }
