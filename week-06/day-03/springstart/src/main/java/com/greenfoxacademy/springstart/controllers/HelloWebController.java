664f56d7954dfc459fddfdd8f5d099d65d5df28c
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-04-16 15:55:41 +0200

    expanded helloworldwebapp into Web Greet counter app

--- week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
+  private int counter = 0;
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam String name) {
+    counter++;
     model.addAttribute("name", name);
+    model.addAttribute("loadCounter", counter);
     return "greeting";
   }
 }
