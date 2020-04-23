f10e386c147c2deabb717aa07d32d5320442e015
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-04-23 13:45:22 +0200

    project setup and first exercise

---
+++ week-07/day-04/src/main/java/com/greenfoxacademy/dependencyinjection/DependencyinjectionApplication.java
@@ -0,0 +1,27 @@
+package com.greenfoxacademy.dependencyinjection;
+
+import com.greenfoxacademy.dependencyinjection.hellodi.model.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyinjectionApplication implements CommandLineRunner {
+
+  private Printer printer;
+
+  @Autowired
+  DependencyinjectionApplication(Printer printer) {
+    this.printer = printer;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DependencyinjectionApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello");
+  }
+}
