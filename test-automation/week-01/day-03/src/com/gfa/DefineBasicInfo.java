d44db56323ba201e06d88e02a6febb1e37467b67
Barna Keszler <keszler.barnabas@gmail.com>
2021-06-10 09:55:12 +0200

    exercise: add DefineBasicInfo

---
+++ test-automation/week-01/day-03/src/com/gfa/DefineBasicInfo.java
@@ -0,0 +1,23 @@
+package com.gfa;
+
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    // Define several things as a variable then print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Whether you are married or not as a boolean
+    String name = "Keszler Barna";
+    int age = 26;
+    double height = 1.97;
+    boolean isMarried = false;
+
+    System.out.println(
+        "Név: " + name + "\n" +
+        "Kor: " + age + "\n" +
+        "Magasság: " + height + "\n" +
+        "Házas-e: " + isMarried
+    );
+
+  }
+}
