b24f450e3fdbec64524ea29f8e157e03906b7fa2
Barna Keszler <keszler.barnabas@gmail.com>
2021-06-11 10:16:33 +0200

    excercise: add IWontCheatOnTheExams

---
+++ test-automation/week-01/day-03/src/com/gfa/IWontCheatOnTheExams.java
@@ -0,0 +1,11 @@
+package com.gfa;
+
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    for (int i = 0; i < 100; i++) {
+      System.out.println((i + 1) + ": I won't cheat on the exam!");
+    }
+  }
+}
