b928bd9af7cfe5ce6c76b04bcb43a40524bce0dc
Barna Keszler <keszler.barnabas@gmail.com>
2021-06-16 09:11:35 +0200

    exercise: add doubling

---
+++ test-automation/week-02/day-03/src/com/gfa/Doubling.java
@@ -0,0 +1,17 @@
+package com.gfa;
+
+public class Doubling {
+  public static void main(String[] args) {
+    // - Create an integer variable named `baseNumber` and assign the value `123` to it
+    // - Create a function called `doubleNumber()` that doubles its integer input parameter
+    //   and returns the doubled value
+    // - Print the result of `doubleNumber(baseNumber)`
+
+    int baseNumber = 123;
+    System.out.println(doubleNumber(baseNumber));
+  }
+
+  public static int doubleNumber(int toDouble) {
+    return toDouble * 2;
+  }
+}
