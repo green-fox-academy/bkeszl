dff4be9aecfbbf539a07aeee27aeb3d6f3d7dbe2
Barna Keszler <keszler.barnabas@gmail.com>
2021-06-16 11:26:23 +0200

    refactor: fix Anagram

--- test-automation/week-02/day-03/src/com/gfa/Anagram.java
+++ test-automation/week-02/day-03/src/com/gfa/Anagram.java
@@ -1,18 +1,28 @@
 package com.gfa;
 
+import java.util.Arrays;
+
 public class Anagram {
   public static void main(String[] args) {
     System.out.println(isAnagram("dog", "god"));
     System.out.println(isAnagram("green", "fox"));
+    System.out.println(isAnagram("listen", "silent"));
+
   }
 
   public static boolean isAnagram(String input1, String input2) {
-   char[] chArr = input1.toCharArray();
-   // input1 -> 'd', 'o', 'g'
-    StringBuilder reverseInput1 = new StringBuilder();
-    for (int i = chArr.length - 1; i >= 0; i--) {
-      reverseInput1.append(chArr[i]);
-    }
-    return reverseInput1.toString().equals(input2);
+   char[] in1ChArr = input1.toCharArray();
+   char[] in2ChArr = input2.toCharArray();
+
+    Arrays.sort(in1ChArr);
+    Arrays.sort(in2ChArr);
+
+    return Arrays.equals(in1ChArr, in2ChArr);
+    //    input1 -> 'd', 'o', 'g'
+    //    StringBuilder reverseInput1 = new StringBuilder();
+    //    for (int i = chArr.length - 1; i >= 0; i--) {
+    //      reverseInput1.append(chArr[i]);
+    //    }
+    //    return reverseInput1.toString().equals(input2);
   }
 }
