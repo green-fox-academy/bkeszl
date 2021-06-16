757d8b787cf39aaf1abadccb1d7811d761442df9
Barna Keszler <keszler.barnabas@gmail.com>
2021-06-16 09:57:56 +0200

    exercise: add factorial

---
+++ test-automation/week-02/day-03/src/com/gfa/Factorial.java
@@ -0,0 +1,20 @@
+package com.gfa;
+
+public class Factorial {
+  public static void main(String[] args) {
+    // - Create a function called `calculateFactorial()`
+    //   that returns the factorial of its input
+
+    System.out.println(calculateFactorial(12));
+  }
+
+  public static long  calculateFactorial(int toFactor) {
+    long result = toFactor;
+
+    //i = 11 -> 10, 9 ... 1
+    for (int i = toFactor - 1; i >= 1 ; i--) {
+      result *= i;
+    }
+    return result;
+  }
+}
