89dc6576b8ca31f4f8c664d72c2eb90cc5c39e1c
Barna Keszler <keszler.barnabas@gmail.com>
2021-06-22 09:25:25 +0200

    exercise: add UrlFixer

---
+++ test-automation/week-03/day-02/src/com/gfa/UrlFixer.java
@@ -0,0 +1,19 @@
+package com.gfa;
+
+public class UrlFixer {
+  public static void main(String... args) {
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crucial component, find out what it is and insert it too!
+    // Try to solve it more than once using different String functions!
+
+    //insert : after the protocol
+    url = url.substring(0, 5) + ":" + url.substring(5);
+
+    //replace bots with odds
+    url = url.replace("bots", "odds");
+
+    System.out.println(url);
+  }
+}
